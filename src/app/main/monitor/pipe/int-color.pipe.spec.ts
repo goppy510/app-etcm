64ab65e0cfe18fd3ba805563485ad31967b69eca
goppy <tacumin.goppy.06@gmail.com>
2025-05-02 21:55:54 +0900

    不要ファイル削除

--- src/app/main/monitor/pipe/int-color.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { IntColorPipe } from './int-color.pipe';
-
-describe('IntColorPipe', () => {
-  it('create an instance', () => {
-    const pipe = new IntColorPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-
-  OAUTH_REDIRECT_URI: 'https://apps.dmdata.ne.jp/etcm/oauth'
-};
